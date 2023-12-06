cbb2f26d3a761ad823f363727b98457fc8fa1f44
Denny Kluge <denny.kluge@axa.de>
2023-12-06 15:41:59 +0100

    started 2023 / day 01 / task 2 - not quite there yet

--- src/main/kotlin/year2023/day01/Day01.kt
+++ src/main/kotlin/year2023/day01/Day01.kt
@@ -11,16 +11,16 @@ var digitMap = mapOf(
     Pair("six", 6),
     Pair("seven", 7),
     Pair("eight", 8),
-    Pair("nine", 9))
+    Pair("nine", 9)
+)
 
 fun main() {
-    val filename = "input_2023_day01.txt"
-    println("day 01 - task 1: ${task1(filename, "main")}")
+    val list = Utils().readFileAsStringList("input_2023_day01.txt", "main")
+//    println("day 01 - task 1: ${task1(list)}")
+    println("day 01 - task 2: ${task2(list)}")
 }
 
-fun task1(filename: String, mainOrTest: String): Int {
-    val list = Utils().readFileAsStringList(filename, mainOrTest)
-
+fun task1(list: List<String>): Int {
     var sum = 0
     for (string in list) {
         val firstDigit = string.firstOrNull { it.isDigit() }
@@ -30,4 +30,56 @@ fun task1(filename: String, mainOrTest: String): Int {
     }
 
     return sum
+}
+
+
+fun task2(list: List<String>): Int {
+    var sum = 0
+    for (string in list) {
+        val firstDigit = getFirstDigitOrWordDigitInString(string)
+        val lastDigit = getLastDigitOrWordDigitInString(string)
+        sum += "$firstDigit$lastDigit".toInt()
+    }
+
+    return sum
+}
+
+
+private fun getFirstDigitOrWordDigitInString(string: String): Int {
+    var currentIndex = Int.MAX_VALUE
+    var currentDigitWord = ""
+    for (digitWord in digitMap.keys) {
+        val indexOf = string.indexOf(digitWord)
+        if (indexOf in 0..<currentIndex) {
+            currentIndex = indexOf
+            currentDigitWord = digitWord
+        }
+    }
+
+    val firstOrNull = string.firstOrNull { it.isDigit() }
+    return if (firstOrNull != null && string.indexOf(firstOrNull) < currentIndex) {
+        firstOrNull.digitToInt()
+    } else {
+        digitMap[currentDigitWord]!!
+    }
+}
+
+
+private fun getLastDigitOrWordDigitInString(string: String): Int {
+    var currentIndex = 0
+    var currentDigitWord = ""
+    for (digitWord in digitMap.keys) {
+        val indexOf = string.lastIndexOf(digitWord)
+        if (indexOf > currentIndex) {
+            currentIndex = indexOf
+            currentDigitWord = digitWord
+        }
+    }
+
+    val lastOrNull = string.lastOrNull { it.isDigit() }
+    return if (lastOrNull != null && string.indexOf(lastOrNull) >= currentIndex) {
+        lastOrNull.digitToInt()
+    } else {
+        digitMap[currentDigitWord]!!
+    }
 }
